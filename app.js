7b54d06080a50bcc1a1587da44fb797a777cbead
Josué Correia <71993090+devjosuecorreia@users.noreply.github.com>
2025-03-17 16:13:36 +0000

    Adiciona funções de exibir o nome adicionado na tela, de atualizar lista de exibição e de Narração.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Essa variável serve para salvar os nomes dos amigos
 let amigos = [];
 
 //Essa função adiciona um amigo a lista
@@ -6,7 +5,7 @@ function adicionarAmigo() {
   let inputAmigo = document.querySelector("#amigo");
   let nome = inputAmigo.value.trim();
 
-  //Alerta, caso o nenhum nome seja adicionado ao formulário
+  //este alerta é ativado caso o nenhum nome seja adicionado ao formulário
   if (nome === "") {
     alert("Por favor, insira um nome.");
     return;
@@ -15,4 +14,34 @@ function adicionarAmigo() {
   amigos.push(nome);
   atualizarListaAmigos();
   inputAmigo.value = "";
+}
+
+//Essa função exibe o texto na tela além de adicionar a narração do recurso responsiveVoice.
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+  responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.1 });
+}
+
+//Essa função atualiza a lista de amigos exibida na tela
+function atualizarListaAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
+
+// Função para sortear um amigo secreto
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Nenhum amigo para sortear.");
+    return;
+  }
+
+  let selecionaSorteado = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[selecionaSorteado];
+  document.getElementById(
+    "resultado"
+  ).innerHTML = `Seu amigo secreto é: ${amigoSorteado}`;
 }
